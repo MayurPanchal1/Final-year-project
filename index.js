43f05da70bdafe61b8f6245d9fbe79744457cf03
Mayur Panchal <mayurpanchal527@gmail.com>
2024-02-13 16:04:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+mongoose.connect('mongodb+srv://mayurpanchal527:Madara%402002@cluster0.zejgeu7.mongodb.net/ecommerce'
+, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+const orderSchema = new mongoose.Schema({
+  customerName: String,
+  productName: String,
+  quantity: Number,
+});
+
+const Order = mongoose.model('Order', orderSchema);
+
+
+app.use(bodyParser.json());
+
+
+app.get('/orders', async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.json(orders);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/orders', async (req, res) => {
+  try {
+    const { customerName, productName, quantity } = req.body;
+    const order = new Order({ customerName, productName, quantity });
+    await order.save();
+    res.status(201).json(order);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
